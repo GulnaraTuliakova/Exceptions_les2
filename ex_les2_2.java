9d85c636013bd07ab600802342675d79cf9db853
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-12-02 20:18:37 +0500

    Add ex2_2

---
+++ ex_les2_2.java
@@ -0,0 +1,23 @@
+// 2. Если необходимо, исправьте данный код 
+// (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
+// try {
+//     int d = 0;
+//     double catchedRes1 = intArray[8] / d;
+//     System.out.println("catchedRes1 = " + catchedRes1);
+//  } catch (ArithmeticException e) {
+//     System.out.println("Catching exception: " + e);
+//  }
+ 
+public class ex_les2_2 {
+    public static void main(String[] args) {
+        try {
+            int[] intArray = new int[] { 15, 8, 1, 7, 9, 45, 7, 6, 9, 74 };
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+}
